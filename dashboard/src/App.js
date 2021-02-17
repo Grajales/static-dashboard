489981711380adc8e15396b6ebe74939bfe87a69
LG29878 <grajalesliliana@johndeere.com>
2021-02-16 22:37:42 -0500

    dashboard area distribution

---
+++ dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="grid-container">
+        <div class="box two">Reviews<br/> 1,281 </div>
+        <div class="box three">Average Rating <br/> 4.6 </div>
+        <div class="box four">Sentiment Analysis<br/>960 <br/>122 <br/>321 </div>
+        <div class="bottombox five">Website Visitors <br/> 821
+          <div class="insidebox"> </div>
+        </div>
+        <div class="sidebox One"> Dashboard <br/>
+                                  Widget <br/>
+                                  Reviews <br/>
+                                  Customers <br/>
+                                  Online Analysis <br/>
+                                  Settings <br/>
+        </div>
+      </div>
+    </div> 
+  );
+}
+
+export default App;
